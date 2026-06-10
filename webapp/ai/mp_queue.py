9abb3cac1a12418d21e5211ad909e42c5027345e
Manju180301 <manju@texainnovates.com>
2026-06-10 15:16:48 +0530

    cleared

--- webapp/ai/mp_queue.py
+++
@@ -1,3 +0,0 @@
-from multiprocessing import Queue
-
-MP_FRAME_QUEUE = Queue(maxsize=1)
--- webapp/core/bridge.py
+++
@@ -1,7 +0,0 @@
-from webapp.camera.camera_reader import FRAME_QUEUE
-from webapp.ai.mp_queue import MP_FRAME_QUEUE
-
-def bridge_frames():
-    while True:
-        cam_id, frame = FRAME_QUEUE.get()
-        MP_FRAME_QUEUE.put((cam_id, frame))
